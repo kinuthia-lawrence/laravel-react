e0dc610f7becec2120fefae2ce33dd7164d2d6e4
Kinuthia Lawrence <kinuthialawrence343@gmail.com>
2025-06-17 08:15:04 +0300

    Initial commit; backend set

---
+++ backend/routes/api.php
@@ -0,0 +1,13 @@
+ <?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+// routes/api.php
+Route::get('/test', function () {
+    return response()->json(['message' => 'Hello from API']);
+});
